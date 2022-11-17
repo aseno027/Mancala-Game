75c505f8fc0d6c069d77699757b1123cba022f01
soundy0115 <soundy0115@gmail.com>
2022-11-17 11:19:38 -0800

    modified constructor of Model

--- src/Model.java
+++ src/Model.java
@@ -6,5 +6,10 @@ public class Model {
 	private ArrayList<ChangeListener> listeners;
 	private ArrayList<Slots> slots;
 	
+	public Model(){
+		listeners = new ArrayList<ChangeListener>();
+		slots = new ArrayList<Slots>();
+		slots.add(new Slots()); // counstruct 1upper 1lower slots
+	}
 	
 }
