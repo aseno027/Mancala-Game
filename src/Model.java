9377583cff6c48c79216490417d6558af1fa619d
soundy0115 <soundy0115@gmail.com>
2022-11-17 11:09:16 -0800

    added model items

---
+++ src/Model.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+import javax.swing.event.ChangeListener;
+
+public class Model {
+	private ArrayList<ChangeListener> listeners;
+	private ArrayList<Slots> slots;
+	
+	
+}
