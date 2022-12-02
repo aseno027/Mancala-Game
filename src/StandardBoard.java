c43580fd04d6ececb30cc26a65cdc408d9ad7697
Juhan Lee <69425103+soundy0115@users.noreply.github.com>
2022-12-01 19:41:37 -0800

    Update StandardBoard.java
    created pits[] to contain pit buttons (Mancala is 0 and 7th)

--- src/StandardBoard.java
+++ src/StandardBoard.java
@@ -1,11 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.geom.*;
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 //CONCRETE STRATEGY
 public class StandardBoard implements MancalaBoard {
@@ -16,6 +18,7 @@ public class StandardBoard implements MancalaBoard {
 	 */
 	public void generateBoard() {
 
+		
 		ImageFiles img = new ImageFiles();
 		
 		JFrame boardFrame = new JFrame();
@@ -42,79 +45,34 @@ public class StandardBoard implements MancalaBoard {
 		boardFrame.setSize(1000, 600);
 		boardFrame.setVisible(true);
 
-		RoundButton pitA1 = new RoundButton(null);
-		RoundButton pitA2 = new RoundButton(null);
-		RoundButton pitA3 = new RoundButton(null);
-		RoundButton pitA4 = new RoundButton(null);
-		RoundButton pitA5 = new RoundButton(null);
-		RoundButton pitA6 = new RoundButton(null);
-		RoundButton pitB1 = new RoundButton(null);
-		RoundButton pitB2 = new RoundButton(null);
-		RoundButton pitB3 = new RoundButton(null);
-		RoundButton pitB4 = new RoundButton(null);
-		RoundButton pitB5 = new RoundButton(null);
-		RoundButton pitB6 = new RoundButton(null);
-		RoundButton trenchA = new RoundButton(null);
-		RoundButton trenchB = new RoundButton(null);
 		
 		
+		JButton[] pits = new JButton[14];
+				
+		for(int i = 0; i < pits.length; i++) {
+			pits[i] = new RoundButton(null);
+		}
 		
-		pitA1.setBackground(Color.WHITE);
-		pitA2.setBackground(Color.WHITE);
-		pitA3.setBackground(Color.WHITE);
-		pitA4.setBackground(Color.WHITE);
-		pitA5.setBackground(Color.WHITE);
-		pitA6.setBackground(Color.WHITE);
-		pitB1.setBackground(Color.WHITE);
-		pitB2.setBackground(Color.WHITE);
-		pitB3.setBackground(Color.WHITE);
-		pitB4.setBackground(Color.WHITE);
-		pitB5.setBackground(Color.WHITE);
-		pitB6.setBackground(Color.WHITE);
+		for(int i = 0; i < pits.length; i++) {
+			pits[i].setBackground(Color.WHITE);
+			pits[i].setBorderPainted(false);
+		}
 		
-		pitA1.setBorderPainted(false);
-		pitA2.setBorderPainted(false);
-		pitA3.setBorderPainted(false);
-		pitA4.setBorderPainted(false);
-		pitA5.setBorderPainted(false);
-		pitA6.setBorderPainted(false);
-		pitB1.setBorderPainted(false);
-		pitB2.setBorderPainted(false);
-		pitB3.setBorderPainted(false);
-		pitB4.setBorderPainted(false);
-		pitB5.setBorderPainted(false);
-		pitB6.setBorderPainted(false);
+		for(int i = 0; i < pits.length; i++) {
+			if(i == 0 || i == 7) {
+				pits[i].setIcon(img.getMancalaImg(0));
+			} else {
+				pits[i].setIcon(img.getPitImg(i));
+			}
+		}
 		
-		pitA1.setIcon(img.getPitImg(0));
-		pitA2.setIcon(img.getPitImg(0));
-		pitA3.setIcon(img.getPitImg(0));
-		pitA4.setIcon(img.getPitImg(0));
-		pitA5.setIcon(img.getPitImg(0));
-		pitA6.setIcon(img.getPitImg(0));
-		pitB1.setIcon(img.getPitImg(0));
-		pitB2.setIcon(img.getPitImg(0));
-		pitB3.setIcon(img.getPitImg(0));
-		pitB4.setIcon(img.getPitImg(0));
-		pitB5.setIcon(img.getPitImg(0));
-		pitB6.setIcon(img.getPitImg(0));
-		
-		
-		trenchA.setIcon(img.getMancalaImg(0));
-		trenchB.setIcon(img.getMancalaImg(0));
-		
-		trenchA.setBackground(Color.WHITE);		
-		trenchB.setBackground(Color.WHITE);
-		
-		trenchA.setBorderPainted(false);
-		trenchB.setBorderPainted(false);
 		
 		JLabel label;
-		centerBoard.add(pitA1);
-		centerBoard.add(pitA2);
-		centerBoard.add(pitA3);
-		centerBoard.add(pitA4);
-		centerBoard.add(pitA5);
-		centerBoard.add(pitA6);
+		for(int i = 8; i < 14; i++) {
+			centerBoard.add(pits[i]);
+		}
+
+		
 		label = new JLabel("B6", SwingConstants.CENTER);
 		label.setFont(new Font("Monospaced", Font.BOLD, 30));
 		centerBoard.add(label);
@@ -152,21 +110,20 @@ public class StandardBoard implements MancalaBoard {
 		label = new JLabel("A6", SwingConstants.CENTER);
 		label.setFont(new Font("Monospaced", Font.BOLD, 30));
 		centerBoard.add(label);
-		centerBoard.add(pitB1);
-		centerBoard.add(pitB2);
-		centerBoard.add(pitB3);
-		centerBoard.add(pitB4);
-		centerBoard.add(pitB5);
-		centerBoard.add(pitB6);
+		
+		for(int i = 1; i < 7; i++) {
+			centerBoard.add(pits[i]);
+		}
+		
 
 		label = new JLabel("Mancala A", SwingConstants.CENTER);
 		label.setFont(new Font("Monospaced", Font.BOLD, 30));
 		mancalaA.add(label, BorderLayout.SOUTH);
-		mancalaA.add(trenchA, BorderLayout.CENTER);
+		mancalaA.add(pits[7], BorderLayout.CENTER);
 		label = new JLabel("Mancala B", SwingConstants.CENTER);
 		label.setFont(new Font("Monospaced", Font.BOLD, 30));
 		mancalaB.add(label, BorderLayout.NORTH);
-		mancalaB.add(trenchB, BorderLayout.CENTER);
+		mancalaB.add(pits[0], BorderLayout.CENTER);
 	}
 
 }
