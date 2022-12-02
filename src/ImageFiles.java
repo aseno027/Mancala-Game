7c032f35c403050cc194c479c3efbc73109f7203
Juhan Lee <69425103+soundy0115@users.noreply.github.com>
2022-12-02 11:25:10 -0800

    Javadoc

--- src/ImageFiles.java
+++ src/ImageFiles.java
@@ -1,10 +1,22 @@
 import javax.swing.ImageIcon;
 
-
+/**
+ * Class that helps other classes to access image files.
+ * 
+ * @author	Team Bagle
+ * @version	1.0.1
+ */
 public class ImageFiles {
 	private ImageIcon[] pitImages;
 	private ImageIcon[] mancalaImages;
 	
+	/**
+	 * Constructor
+	 * - pitImages will be initialized with pit images.
+	 * - mancalaImages will be initialized with mancala images.
+	 * - Each array's index means the number of stones in pit or mancala.
+	 * - 0 index is storing the empty image of pit or mancala.
+	 */
 	public ImageFiles() {
 		pitImages = new ImageIcon[21];
 		mancalaImages = new ImageIcon[26];
@@ -29,10 +41,23 @@ public class ImageFiles {
 	}
 	
 	
+	/**
+	 * Method that returns pit image with stones
+	 * 
+	 * @param	stoneNum			number of stones in pit
+	 * @return	pitImages[stoneNum]	pit image with number of stones that parameter passed
+	 */
 	public ImageIcon getPitImg(int stoneNum) {
 		return pitImages[stoneNum];
 	}
 	
+	
+	/**
+	 * Method that returns mancala image with stones
+	 * 
+	 * @param	stoneNum				number of stones in mancala
+	 * @return	mancalaImages[stoneNum]	mancala image with number of stones that parameter passed
+	 */
 	public ImageIcon getMancalaImg(int stoneNum) {
 		return mancalaImages[stoneNum];
 	}
